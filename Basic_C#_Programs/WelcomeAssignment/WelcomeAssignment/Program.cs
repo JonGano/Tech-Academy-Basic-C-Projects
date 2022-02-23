839a006542ab7e3ff12363e3a731c1a164cb8305
JonGano <97046218+JonGano@users.noreply.github.com>
2022-02-23 11:59:48 -0500

    utilizing escape sequences for a printed string

---
+++ Basic_C#_Programs/WelcomeAssignment/WelcomeAssignment/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welecome to Acme Accounting Systems \nRemember, we're \"accounting\" on you!");
+        Console.ReadLine();
+    }
+}
+
