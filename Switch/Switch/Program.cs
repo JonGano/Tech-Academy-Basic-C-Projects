d55fefde66af5e5e589b5e7d3d5e2269e213f9b2
JonGano <97046218+JonGano@users.noreply.github.com>
2022-03-01 15:15:34 -0500

    Added comments

---
+++ Switch/Switch/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int day = 27;
+            //switch (day)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Monday");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Tuesday");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Wednesday");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Thursday");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Friday");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Saturday");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Sunday");
+            //        break;
+            //    default:
+            //        Console.WriteLine( "What country are you from? Do they speak english in What? Say what again! I dare you! I double dare you! Say what one more time!");
+            //        break;
+            //}
+            //Console.ReadLine();
+
+            //Console.WriteLine("Guess a number?");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //bool isGuessed = false; // you could write isGuessed = number == 12; here the number either does or doesnt ==12 making it assignable to a boolean variable.
+            //                        // if you write it this way and guess right on the frist try it wont print mssge because it never enters the while loop.
+            //                        //this can be corrected wit ha do while loop see bellow.... although while loops are more common than do while loops
+            //while (isGuessed == false) // and (!isGuessed)this is saying while variable == false if it's a boolean variable.
+            //{
+            //    switch (number)
+            //    {
+            //        case 62:
+            //            Console.WriteLine("You guessed 62. try again.");
+            //            Console.WriteLine("Guess a number?");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 29:
+            //            Console.WriteLine("You guessed 29. try again.");
+            //            Console.WriteLine("Guess a number?");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 55:
+            //            Console.WriteLine("You guessed 55. try again.");
+            //            Console.WriteLine("Guess a number?");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 12:
+            //            Console.WriteLine("You guessed 12. That is correct!");
+            //            isGuessed = true;
+            //            break;
+            //        default:
+            //            Console.WriteLine("You are wrong");
+            //            Console.WriteLine("Guess a number?");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //    }
+            //}
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = false;
+
+            do
+            {
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62. try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29. try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55. try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed 12. That is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (isGuessed == false); // and (!isGuessed)this is saying while variable == false if it's a boolean variable.
+            
+            Console.ReadLine();
+        }
+    }
+}
+       
--- stringsAndIntegersAssignment/stringsAndIntegersAssignment/Program.cs
+++ stringsAndIntegersAssignment/stringsAndIntegersAssignment/Program.cs
@@ -12,33 +12,34 @@ class Program
         try
         {
             List<int> divlist = new List<int>() { 20, 30, 50, 90, 150, 64, 10, 100, 78 };
-
+            // a number is promted by the user and assigned to divide the items of the integer list above
             Console.WriteLine("Please enter a number to divide each list integer by");
+            //converts user input and assigns integer to variable div
             int div = Convert.ToInt32(Console.ReadLine());
-
+            //foreach executes the division equation on each number of the list
             foreach (int num in divlist)
             {
                 Console.WriteLine(num / div);
             }
             Console.ReadLine();
         }
-            catch (FormatException ex)
+            catch (FormatException ex)//catch if an invalid string is entered that cannot be converted into an integer
             {
                 Console.WriteLine(ex.Message);
             }
-            catch (DivideByZeroException ex)
+            catch (DivideByZeroException ex)//catch for an attempt at dividing by zero
             {
                 Console.WriteLine(ex.Message);
             }
-            catch (OverflowException ex)
+            catch (OverflowException ex)//catch for too many characters being entered by user
             {
                 Console.WriteLine(ex.Message);
             }
-            finally
+            finally //ensures the console can be read if execution is stopped
             {
                 Console.ReadLine();
             }
-            Console.WriteLine("this displays outside of the try catch blocks");
+            Console.WriteLine("this displays outside of the try catch blocks");//this prints to the console after all try catch blocks are done executing
             Console.ReadLine();
         }
     }
