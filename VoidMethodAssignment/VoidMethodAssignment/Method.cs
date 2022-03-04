0fd96881478fccde0799c3023d081a892ff6da31
JonGano <97046218+JonGano@users.noreply.github.com>
2022-03-04 14:20:10 -0500

    void method

---
+++ VoidMethodAssignment/VoidMethodAssignment/Method.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssignment
+{
+    class Method
+    {
+        public void MyMethod(int x, int y)//method void - doesn't return anything
+        {
+            x += 5; //math operation that doesn't return or is ever seen by anything
+            Console.WriteLine(y);//prints the y variable on the screen
+        }
+    }
+}
---
+++ VoidMethodAssignment/VoidMethodAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method num = new Method();//instanciates an object of the method class named num
+
+            num.MyMethod(3, 4);//called the method and gave it parameters
+
+            num.MyMethod(x: 2, y: 4); //Specified parameters by name in the method
+
+            Console.ReadLine();
+         
+        }
+    }
+}
