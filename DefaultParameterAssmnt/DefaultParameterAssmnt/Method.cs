381715dd79ba241f5dbe0dde801a53130bbfaca2
JonGano <97046218+JonGano@users.noreply.github.com>
2022-03-04 11:49:11 -0500

    slide 280 assignment
    allowed user to choose either one or two arguments to pass through a method by setting a default value for one parameter. using a conditional statement to determine whether or not the second potential argument was left blank.

---
+++ DefaultParameterAssmnt/DefaultParameterAssmnt/Method.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultParameterAssmnt
+{
+    class Method
+    {
+        public int Add(int x, int y = 2)//method is given two int parameters and assigns one a default value
+        {
+            return x + y;//returns the math operation x + y, if there is no argument passed for y the method will still operate because of it's default value
+        }
+    }
+}
---
+++ DefaultParameterAssmnt/DefaultParameterAssmnt/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultParameterAssmnt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method num = new Method(); //instanciates an object named num from the Method class num.(method)() is used to carry out the mehtod
+
+            Console.WriteLine("Please enter a number for my method");//asks user for a number
+            int x = Convert.ToInt32(Console.ReadLine());//converts from string to int and assigned to variable x
+
+            Console.WriteLine("Please enter another number for my method if you care to");//gives the user a choice to enter another number
+            string q = Console.ReadLine();//that string is assigned to the varable q
+
+            if (q != "")//if the user chooses to enter a second number and the string they enter is not ""(void of characters)
+            {
+                int y = Convert.ToInt32(q);//string variable q is converted to an integer and assigned to the int varable y
+                Console.WriteLine("Your first number and your second number added together are " + num.Add(x, y));//both x and y are passed as arguments to the method
+            }
+            else  // y is not assigned a value and only one argument from the user passes through the method
+            {
+                Console.WriteLine("Your number plus two is " + num.Add(x));
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+    
+
