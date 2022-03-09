eb66ae7599792a52dfc286197f8393da039a2b28
JonGano <97046218+JonGano@users.noreply.github.com>
2022-03-08 19:55:47 -0500

    Created a struct object

---
+++ structAssignment/structAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num;//instantiates num object from the struct type Number
+
+            num.Amount = 42;//assigns 42 vto the amount property of the Number struct object num
+
+
+            Console.WriteLine(num.Amount);//print the amount property in the console
+            Console.ReadLine();
+        }
+      
+    }
+}
