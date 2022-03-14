0f1ce3dcb235105993fe8d1c534df4894dbac5b5
JonGano <97046218+JonGano@users.noreply.github.com>
2022-03-14 14:13:42 -0400

    Added current date

--- DateTimeAssignment/DateTimeAssignment/Program.cs
+++ DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -9,17 +9,17 @@ namespace DateTimeAssignment
     class Program
     {
         static void Main(string[] args)
-        {
-          
-        Console.WriteLine("Please enter a whole number");
-            int x = Convert.ToInt32(Console.ReadLine());//took a number from the user's input and gave the vallue to x
+        {   //prints the current date and time to the console
+            Console.WriteLine("It is currently: " + DateTime.Now + "\n");
+        Console.WriteLine("Please enter a whole number");//converted user input to int and
+            int x = Convert.ToInt32(Console.ReadLine());//and gave the int value to x
            
-            //DateTime now = DateTime.Now;//made a dateTime variable with the value of DateTime.Now
-            //DateTime variable z is assigned the value of current time + x hours
-            DateTime z = DateTime.Now.AddHours(x);//used the .Now method to get the current time value.
-             //added hours to the current time by passing the user's number into the AddHours() method as an argument
+            
+                 //DateTime variable z is assigned the value of current time + x hours
+            DateTime z = DateTime.Now.AddHours(x);      //used the .Now method to get the current time value.
+                                    //added hours to the current time by passing the user's number into the AddHours() method as an argument
 
-            Console.WriteLine("it will be {0}, {1} hours from now", z, x);//printed the result to the console
+            Console.WriteLine("It will be {0}, {1} hour(s) from now", z, x);//printed the result to the console
             Console.ReadLine();
         }
     }
