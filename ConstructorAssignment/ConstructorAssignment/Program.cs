a3927f8d83e811473dd0cb3e4a1dd5e21ba0ca57
JonGano <97046218+JonGano@users.noreply.github.com>
2022-03-15 15:30:22 -0400

    objected created using constructor call chain

---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string brand = "G.M.C."; //creates a constant
+            var cartype = "1500";//implicit variable instantiation
+
+            automobile farmTruck = new automobile(brand, cartype);//object created using a constructor call chain
+                  //constant and implicit string variable are passed as arguments to the class constructor
+
+            farmTruck.ForSale( 25);//ran class method using farmtruck object
+           
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/automobile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    public class automobile                                                   //: this() - says it's inheriting the constructor attributes from the origional constructor
+    {//constructor chain made with a default for one of the parameters. : this(keeping make parameter, keeping model parameter, defaulting year if not specified)
+        public automobile(string make, string model) : this(make, model, 1985)
+        {
+            //left empty because it's using the origional constructor to assemble the object
+        }
+
+        public automobile(string make, string model, int year)//class constructor created with 3 parameters
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public void ForSale(int speed)//method created to display all object properties
+        {
+            //Console.WriteLine("For sale! " + Year + " " +  Make + " " +  Model + ". Top speed is " + speed + " mph.");
+            Console.WriteLine("For sale! {0} {1} {2}. top speed {3}", Year, Make, Model, speed);
+        }
+    }
+}
