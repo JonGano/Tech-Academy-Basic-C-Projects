c2b4c830580d96c47e42894ad2a94332f2179f0d
JonGano <97046218+JonGano@users.noreply.github.com>
2022-03-30 15:20:47 -0400

    created a web app that displays UTC in the browser

---
+++ ServerTimeAssignment/ServerTimeAssignment/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServerTimeAssignment.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Message { get; set; }//instantiated message property for use in onget method
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        
+        public void OnGet()//onget method sends data to cshtmlpage
+        {//this keyword points to the object for which the method is called
+            this.Message = DateTime.UtcNow.ToString();
+        }
+
+
+    }
+}
